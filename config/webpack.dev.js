4b8627ddbce29cb0103fe65892b06399cc6a4663
Alibh Deori <alibh619@gmail.com>
2018-06-19 17:10:17 +0530

    added comments

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,13 +4,13 @@ module.exports = {
   entry: {
     main: './src/main.js'
   },
-  mode: "development",
+  mode: "development", // Two modes avaliable "production" and "developemnt"
   output: {
-    filename: "[name]-bundle.js",
+    filename: "[name]-bundle.js", // [name] is the name of the file
     path: path.resolve(__dirname, "../dist"),
     publicPath: "/js/"
   },
   devServer: {
-    contentBase: "dist"
+    contentBase: "dist" // Folder from where webpack-dev-derver will server files via http
   }
 }
